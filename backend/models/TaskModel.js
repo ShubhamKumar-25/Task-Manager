a4b273ed025b19e0d85c46725b698ad66a00a259
Shubham <sk5989229@gmail.com>
2026-09-06 12:50:59 +0530

    auth

--- backend/models/TaskModel.js
+++ backend/models/TaskModel.js
@@ -1,7 +1,54 @@
 
+// const mongoose = require("mongoose");
+
+// const taskSchema = mongoose.Schema(
+//     {
+//         title: {
+//             type: String,
+//             required: true,
+//             trim: true
+//         },
+
+//         description: {
+//             type: String,
+//             required: true,
+//             trim: true
+//         },
+
+//         priority: {
+//             type: String,
+//             enum: ["Low", "Medium", "High"],
+//             default: "Medium"
+//         },
+
+//         status: {
+//             type: String,
+//             enum: ["Pending", "In Progress", "Completed"],
+//             default: "Pending"
+//         },
+
+//         dueDate: {
+//             type: Date,
+//             required: true
+//         }
+//     },
+//     {
+//         timestamps: true
+//     }
+// );
+
+// const Task = mongoose.model("Task", taskSchema);
+// module.exports = Task;
+
+
+
+
+
+
+
 const mongoose = require("mongoose");
 
-const taskSchema = mongoose.Schema(
+const taskSchema = new mongoose.Schema(
     {
         title: {
             type: String,
@@ -30,6 +77,12 @@ const taskSchema = mongoose.Schema(
         dueDate: {
             type: Date,
             required: true
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
         }
     },
     {
@@ -38,4 +91,5 @@ const taskSchema = mongoose.Schema(
 );
 
 const Task = mongoose.model("Task", taskSchema);
+
 module.exports = Task;

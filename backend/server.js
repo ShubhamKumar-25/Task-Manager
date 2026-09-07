837780f7d07e92ab938d222e6895de217e96f951
Shubham <sk5989229@gmail.com>
2026-09-07 15:58:42 +0530

    server change

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
 const connectDB = require("./config/db");
-const router = require('./routes/taskRoute')
+
+const taskRouter = require("./routes/taskRoute");
+const authRouter = require("./routes/authRoute");
 
 dotenv.config();
+
 connectDB();
+
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', router)
+
+// Auth routes
+app.use("/api/auth", authRouter);
+
+
+// Task routes
+app.use("/api", taskRouter);
+
 
 app.get("/", (req, res) => {
     res.send("API is running...");
-})
+});
+
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on localhost: ${PORT}`);
-})
+});

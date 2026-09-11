95a37b8168db8909cc1c9e59b3883d3ce7694687
Shubham <sk5989229@gmail.com>
2026-09-11 14:19:38 +0530

    file ptoblem resolved

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const connectDB = require("./config/db");
 
@@ -11,7 +12,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 
